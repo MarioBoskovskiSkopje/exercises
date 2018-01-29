c3cb8386ee2b05a687a6d6a711efe9b5ad4e908c
marioBagwell <33531217+marioBagwell@users.noreply.github.com>
2018-01-29 23:26:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -38,38 +38,53 @@
 
 
 
-function interviewQuestion(job) {
-    if (job === "designer")
+// function interviewQuestion(job) {
+//     if (job === "designer")
 
-    {
-        return function (name) {
-            console.log(name + ", can you please explain what UX design is ?");
-        }
-    } else if (job === "teacher") {
-        return function (name) {
-            console.log("What subject do you teach," + name + " ?");
-        }
+//     {
+//         return function (name) {
+//             console.log(name + ", can you please explain what UX design is ?");
+//         }
+//     } else if (job === "teacher") {
+//         return function (name) {
+//             console.log("What subject do you teach," + name + " ?");
+//         }
 
-    } else {
-        return function (name) {
-            console.log("Hello" + name + ", what do you do?");
-        }
+//     } else {
+//         return function (name) {
+//             console.log("Hello" + name + ", what do you do?");
+//         }
+//     }
+// }
+
+
+// var teacherQuestion = interviewQuestion("teacher");
+
+// var designerQuestion = interviewQuestion("designer");
+
+
+// teacherQuestion("John");
+// designerQuestion("John");
+// designerQuestion("Jane");
+// designerQuestion("Mark");
+// teacherQuestion("John");
+// teacherQuestion("John");
+// teacherQuestion("John");
+
+
+// interviewQuestion("teacher")("Mark");
+
+
+
+function retirement(retirementAge) {
+    var a = " year left until retirement.";
+    return function (yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
     }
 }
 
+var retirementUS = retirement(66);
+retirementUS(1990);
 
-var teacherQuestion = interviewQuestion("teacher");
-
-var designerQuestion = interviewQuestion("designer");
-
-
-teacherQuestion("John");
-designerQuestion("John");
-designerQuestion("Jane");
-designerQuestion("Mark");
-teacherQuestion("John");
-teacherQuestion("John");
-teacherQuestion("John");
-
-
-interviewQuestion("teacher")("Mark");
+retirement(66)(1990);
